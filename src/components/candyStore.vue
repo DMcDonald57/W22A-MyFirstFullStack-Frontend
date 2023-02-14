6571fcb6aa3fca0824b76a6d70db6d52a17b049b
DMcDonald57 <d.mcdonald579@gmail.com>
2023-02-13 21:03:26 -0700

    Final commit

--- src/components/candyStore.vue
+++ src/components/candyStore.vue
@@ -2,9 +2,9 @@
     <div>
         <p>"Everything you need for your candy list"</p>
         <button @click="get_list">Candy List</button>
-        <!-- <button @click="add_candy">Add Candy</button>
+        <button @click="add_candy">Add Candy</button>
         <button @click="change_candy">Change Candy</button>
-        <button @click="delete_candy">Delete Candy</button> -->
+        <button @click="delete_candy">Delete Candy</button>
     </div>
 </template>
 
@@ -25,7 +25,19 @@ import axios from "axios";
                 print(error);
             })
         },
-    }
+        add_Candy() {
+            axios.request({
+                url: 'http://127.0.0.1:5000/api/candy',
+                method : "POST",
+            }).then((response)=> {
+                let src = response;
+                this.$root.$emit(src)
+                print(response)
+            }).catch((error) => {
+                print(error);
+            })
+        },
+    },
 }
 </script>
 
